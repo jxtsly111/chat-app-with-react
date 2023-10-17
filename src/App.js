aad132851e328d2205f37be2905ebe9cf2ccfc94
Your Name <jxtsly@gmail.com>
2023-10-17 11:28:00 +0200

    React-chat-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Pusher from "pusher-js";
 
 function App() {
+  const [username, setUsername] = useState('username');
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  let allMessages = [];
+
+  useEffect(() => {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('6ead2f62b40681bf8348', {
+      cluster: 'mt1'
+    });
+
+    const channel = pusher.subscribe('laravel-chat');
+    channel.bind('message', function (data) {
+     allMessages.push(data);
+     setMessages(allMessages);
+    });
+  }, []);
+
+  const submit = async e => {
+    e.preventDefault();
+
+    await fetch('http://127.0.0.1:8000/api/messages',{
+      method : 'POST',
+      headers : {'Content-Type' : 'application/json'},
+      body : JSON.stringify({
+        username,
+        message
+      })
+    });
+    setMessage('');
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-body-tertiary" >
+        <div className="d-flex align-items-center flex-shrink-0 p-3 link-body-emphasis text-decoration-none border-bottom">
+          <svg className="bi pe-none me-2" width="30" height="24">
+            <use xlinkHref="#bootstrap"></use>
+          </svg>
+          <input className="fs-5 fw-semibold" value={username} onChange={e => setUsername(e.target.value)} />
+        </div>
+        <div className="list-group list-group-flush border-bottom scrollarea">
+          {messages.map(message => {
+            return (
+              <div className="list-group-item list-group-item-action py-3 lh-sm">
+                <div className="d-flex w-100 align-items-center justify-content-between">
+                  <strong className="mb-1">{message.username}</strong>
+                </div>
+                <div className="col-10 mb-1 small">{message.message}</div>
+              </div>
+            )
+          })}
+
+
+
+        </div>
+      </div>
+      <form onSubmit={e => submit(e)}>
+        <input className="form-control" placeholder="Write a message" value={message} onChange={e => setMessage(e.target.value)} />
+
+      </form>
     </div>
+
   );
 }
 
